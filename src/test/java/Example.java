812bfd4e3b6463acac06b87da0c6fa2c6ebdd315
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2022-05-22 13:28:22 +0600

    test case 03(logout) and test script for this has been written and executed

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,5 +1,5 @@
 
-import java.nio.file.Paths;
+//import java.nio.file.Paths;
 
 import com.microsoft.playwright.*;
 
@@ -8,7 +8,8 @@ public class Example {
 	static Browser browser;
 	static Page page;
 	
-	public static void navigate()
+	//navigate to the homepage
+	public static void TC_001()
 	{
 		playwright = Playwright.create();
 	    browser = playwright.chromium().launch(
@@ -19,15 +20,23 @@ public class Example {
 	    page.navigate("https://demo.guru99.com/v4/index.php");
 	}
 	
-	public static void login()
+	//login
+	public static void TC_002()
 	{
-		navigate();
+		TC_001();
 		page.fill("input[name=\"uid\"]", "mngr408769");
 		page.fill("input[name=\"password\"]", "anasAgA");
 		page.locator("xpath = /html/body/form/table/tbody/tr[3]/td[2]/input[1]").click();
 	}
 	
+	//logout
+	public static void TC_003()
+	{
+		TC_002();
+		page.locator("xpath = /html/body/div[3]/div/ul/li[15]/a").click();
+	}
+	
 	public static void main(String[] args) {
-		login();
+		TC_003();
 	  }
 }
