4782b38ac9e3b2dbf751ad609b4a4318c845658a
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2022-05-22 14:08:31 +0600

    test case 04(edit customer) and test script for this has been written and executed

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -4,11 +4,13 @@
 import com.microsoft.playwright.*;
 
 public class Example {
+	
 	static Playwright playwright;
 	static Browser browser;
 	static Page page;
 	
-	//navigate to the homepage
+	/** navigate to the homepage */
+	
 	public static void TC_001()
 	{
 		playwright = Playwright.create();
@@ -20,7 +22,8 @@ public class Example {
 	    page.navigate("https://demo.guru99.com/v4/index.php");
 	}
 	
-	//login
+	/** Login */
+	
 	public static void TC_002()
 	{
 		TC_001();
@@ -29,14 +32,28 @@ public class Example {
 		page.locator("xpath = /html/body/form/table/tbody/tr[3]/td[2]/input[1]").click();
 	}
 	
-	//logout
+	/** logout */
+	
 	public static void TC_003()
 	{
 		TC_002();
-		page.locator("xpath = /html/body/div[3]/div/ul/li[15]/a").click();
+		page.locator("xpath = /html/body/div[3]/div/ul/li[15]/a").click(); //click logout button
+	}
+	
+	/** edit customer */
+	
+	public static void TC_004()
+	{
+		TC_002();
+		page.locator("text=Edit Customer").click(); //click edit customer button from left sidebar
+		page.fill("input[name=\"cusid\"]", "22055"); //entry customer id
+		page.locator("xpath = /html/body/div[2]/table/tbody/tr/td/table/tbody/tr[11]/td[2]/input[1]").click(); //click submit
+		page.fill("input[name=\"city\"]", "Barisal"); //edit city
+		page.fill("textarea[name=\"addr\"]", "Pallabi!!!");	 //edit address
+		page.locator("xpath = /html/body/table/tbody/tr/td/table/tbody/tr[13]/td[2]/input[1]").click(); //click submit
 	}
 	
 	public static void main(String[] args) {
-		TC_003();
+		TC_004();
 	  }
 }
